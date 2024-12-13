0400b0534b1989661c52e13f6030a81953b464e1
Ayub Ali <ayubali125212@gmail.com>
2024-12-13 19:38:46 +0300

    FMS-0 Initial Setup.

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <h1 className="text-6xl font-extrabold">Fleet Management System</h1>
+    </div>
+  );
+}
+
+export default App;
