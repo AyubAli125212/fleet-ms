7d228e69a88d902a63e537577aa1eec9fc6e5ae2
Ayub Ali <ayubali125212@gmail.com>
2024-12-15 16:41:17 +0300

    env

--- client/src/api/API.ts
+++ client/src/api/API.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 import { config } from "@/config";
 
-const API_BASE_URL = config.API_URL || "http://localhost:8000";
+const API_BASE_URL = config.API_URL || "https://fleet-ms.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL + "/api/v1",
